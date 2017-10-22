4a7927c3abf733940b1fb0f7636bbd02bf727dae
Manunath Gopalan <32996283+manusmrit@users.noreply.github.com>
2017-10-22 10:03:03 +0200

    Create MiniMeTokenFactory.sol

---
+++ MiniMeTokenFactory.sol
@@ -0,0 +1,35 @@
+pragma solidity ^0.4.17;
+
+/**
+ * @title MiniMeTokenFactory
+ * @dev used to create clone tokens
+ */
+contract MiniMeTokenFactory is Controlled {
+
+    address public _address;
+
+    /* clone creation method */
+    function createCloneToken(
+        address _parentToken,
+        uint _snapshotBlock,
+        string _tokenName,
+        uint8 _decimalUnits,
+        string _tokenSymbol,
+        bool _transfersEnabled
+    ) public returns (MiniMeToken) {
+        MiniMeToken newToken = new MiniMeToken(
+            this,
+            _parentToken,
+            _snapshotBlock,
+            _tokenName,
+            _decimalUnits,
+            _tokenSymbol,
+            _transfersEnabled
+            );
+        
+        _address = address(this);
+        newToken.transferControl(msg.sender);
+        return newToken;
+    }
+    
+}
