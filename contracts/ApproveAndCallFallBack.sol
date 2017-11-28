27ee641647edaaddaf1249f2dd189dffeaa9d079
Manunath Gopalan <32996283+manusmrit@users.noreply.github.com>
2017-11-28 14:45:44 +0200

    Delete ApproveAndCallFallBack.sol

--- contracts/ApproveAndCallFallBack.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.4.17;
-
-/**
- * @title ApproveAndCallFallBack
- * @dev Used to ping receiver of approval
- */
-contract ApproveAndCallFallBack { 
-  function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) public; 
-}
